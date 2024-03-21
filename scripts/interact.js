b768b7180d9834bdb50246c6e3e384b7a7c6070b
Lynx96 <joaohenrique_as@hotmail.com>
2024-03-21 00:47:32 -0300

    Decode data from transaction

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require('fs');
+const abiDecoder = require('abi-decoder')
 require("dotenv").config({ path: path.resolve("../.env") });
 
 const API_KEY = process.env.API_KEY;
@@ -7,7 +8,8 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const { ethers } = require("ethers");
-const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+const contractABI = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
+/* const interface = new ethers.Interface(contractABI); */
 
 //provider - alchemy
 const alchemyProvider = new ethers.AlchemyProvider(
@@ -21,7 +23,7 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 //contract instance
 const helloWorldContract = new ethers.Contract(
   CONTRACT_ADDRESS,
-  contract.abi,
+  contractABI.abi,
   signer
 );
 
@@ -31,12 +33,17 @@ async function main() {
   console.log("The message is: " + message);
 
   console.log("Updating the message...");
-  const tx = await helloWorldContract.update("Sixth attempt!");
+  abiDecoder.addABI(contractABI.abi);
+  const tx = await helloWorldContract.update("Fifteenth attempt!");
   await tx.wait();
   const updateTransactionHash = tx.hash;
   saveTransactionHash(updateTransactionHash, filePath);
+  const txData = tx.data;
 
-  console.log(updateTransactionHash);
+  console.log(tx.data);
+ /*  const decoded = interface.parseTransaction(txData) */
+  const decoded = abiDecoder.decodeMethod(txData);
+  console.log("Decoded data is: ", decoded);
 
   const newMessage = await helloWorldContract.message();
   console.log("The new message is: " + newMessage);
